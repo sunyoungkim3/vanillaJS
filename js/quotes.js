cff2a349f15fa6a143d1ac0e76dc20067491a9e3
김선영 (KIM SUNYOUNG) 과장 (Application SW팀) <sykim3@seegene.com>
2023-05-04 20:05:20 +0900

    quotes.js 변경

--- js/quotes.js
+++ js/quotes.js
@@ -1,51 +1,44 @@
 const quotes = [
   {
-    quote: "aaa",
-    author: "aaabbb",
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
   },
   {
-    quote: "bbb",
-    author: "aaafdsfdsbbb",
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
   },
   {
-    quote: "ccc",
-    author: "aadsaaabbb",
+    quote:
+      "The world is a book and those who do not travel read only one page.",
+    author: "Saint Augustine",
   },
   {
-    quote: "ddd",
-    author: "ssssaaabbb",
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
   },
   {
-    quote: "eee",
-    author: "fffffaaabbb",
+    quote: "To Travel is to Live",
+    author: "Hans Christian Andersen",
   },
   {
-    quote: "fff",
-    author: "gggaaabbb",
+    quote: "Only a life lived for others is a life worthwhile.",
+    author: "Albert Einstein",
   },
   {
-    quote: "aaa",
-    author: "aaabbb",
+    quote: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West",
   },
   {
-    quote: "bbb",
-    author: "aaafdsfdsbbb",
+    quote: "Never go on trips with anyone you do ntot love.",
+    author: "Hemmingway",
   },
   {
-    quote: "ccc",
-    author: "aadsaaabbb",
+    quote: "We wander for distraction, but we travel for fulfilment.",
+    author: "Hilaire Belloc",
   },
   {
-    quote: "ddd",
-    author: "ssssaaabbb",
-  },
-  {
-    quote: "eee",
-    author: "fffffaaabbb",
-  },
-  {
-    quote: "fff",
-    author: "gggaaabbb",
+    quote: "Travel expands the mind and fills the gap.",
+    author: "Sheda Savage",
   },
 ];
 
